d6bde6271d4225b11da1a7eb8cb69752b7f4d99f
yaosai <yao.sai@hotmail.com>
2018-10-17 21:53:06 +0800

    简单事件操作

--- app.js
+++ app.js
@@ -3,12 +3,26 @@ new Vue({
     data: {
         name: "yaosai",
         age: 18,
-        website:"http://www.baidu.com",
-        websiteTag:"<a href='http://www.baidu.com'>绑定标签</a>"
+        website: "http://www.baidu.com",
+        websiteTag: "<a href='http://www.baidu.com'>绑定标签</a>",
+        x: 0,
+        y: 0
     },
     methods: {
         greet: function (name) {
-            return 'Good Mroning! '+name;
+            return 'Good Mroning! ' + name;
+        },
+        add: function (sum) {
+            this.age += sum;
+        },
+        sub: function (sum) {
+            this.age -= sum;
+        },
+        // 获取鼠标所在处的x,y,赋值给x,y
+        updateXY: function (event) {
+            console.log(event);
+            this.x = event.offsetX;
+            this.y = event.offsetY
         }
     }
 })
